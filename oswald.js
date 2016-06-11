97eb2b59f2a5baa9da9841b1fce764cac12dbd8c
Anand Chowdhary <Anand Chowdhary>
2016-06-11 11:48:58 +0530

    Local storage

--- oswald.js
+++ oswald.js
@@ -5,13 +5,33 @@ var _ = function(elt, command, fn) {
 	return elt.addEventListener(command, fn);
 }
 
-_($("#enableOswald"), "click", function() {
+var Oswald = function() {
+	if (localStorage.getItem("serviceStatus") == "start") {
+		Oswald.startService();
+	} else {
+		Oswald.stopService();
+	}
+}
+Oswald.startService = function() {
+	localStorage.setItem("serviceStatus", "start");
 	$(".disabled").style.display = "none";
 	$(".enabled").style.display = "block";
 	$("body").classList.add("on");
-});
-_($("#disableOswald"), "click", function() {
+}
+Oswald.stopService = function() {
+	localStorage.setItem("serviceStatus", "stop");
 	$(".disabled").style.display = "block";
 	$(".enabled").style.display = "none";
 	$("body").classList.remove("on");
+}
+
+_($("#enableOswald"), "click", function() {
+	Oswald.startService();
+});
+_($("#disableOswald"), "click", function() {
+	Oswald.stopService();
+});
+
+_(document, "DOMContentLoaded", function() {
+	Oswald();
 });
