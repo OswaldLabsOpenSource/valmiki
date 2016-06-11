bd9c2880acdab018eb48d718e53d51acf1c1734a
Anand Chowdhary <anandchowdhary@gmail.com>
2016-06-11 19:44:17 +0530

    Enable new styles

--- injection.js
+++ injection.js
@@ -4,7 +4,7 @@ chrome.storage.sync.get({
 	if (items.serviceStatus == "start") {
 		if (document.querySelectorAll("#oswaldSheet").length <= 0) {
 			var externalSheet = document.createElement("link");
-			externalSheet.setAttribute("href", "http://localhost/demo.css");
+			externalSheet.setAttribute("href", "//getoswald.xyz/style1.css");
 			externalSheet.setAttribute("rel", "stylesheet");
 			externalSheet.setAttribute("type", "text/css");
 			externalSheet.setAttribute("id", "oswaldSheet");
