b103a40db63fbbee71c18e4d12e1a6ba1f24c32f
Anand Chowdhary <Anand Chowdhary>
2016-06-11 10:32:45 +0530

    Add stuff

--- oswald.js
+++ oswald.js
@@ -6,5 +6,12 @@ var _ = function(elt, command, fn) {
 }
 
 _($("#enableOswald"), "click", function() {
-	alert("X");
+	$(".disabled").style.display = "none";
+	$(".enabled").style.display = "block";
+	$("body").classList.add("on");
+});
+_($("#disableOswald"), "click", function() {
+	$(".disabled").style.display = "block";
+	$(".enabled").style.display = "none";
+	$("body").classList.remove("on");
 });
